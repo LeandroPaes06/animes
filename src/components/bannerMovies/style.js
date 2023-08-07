bbba2d2d21ad971a6911a9729a7717590317f9de
LeandroPaes06 <leandrinpaes@gmail.com>
2023-08-07 18:02:18 -0300

    Animes

---
+++ src/components/bannerMovies/style.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from "react-native"
+
+
+const styles = StyleSheet.create({
+
+    textBanner:{
+        fontSize:30,
+        color:"white",
+        marginTop:15,
+        marginLeft:-180,
+        fontWeight:'Bold',
+        alignItems: 'center'
+    },
+    imageBanner:{
+        width:'100%',
+        height:200,
+        marginTop:15,
+        borderRadius:10
+    }
+})
+export default styles;
---
+++ src/data/filmes.js
@@ -0,0 +1,65 @@
+const DATA = 
+[
+{
+    id:'01',
+    nome:'Adão Negro',
+    nota:'4.5',
+    imagem:'Naruto.jpg'
+},
+{
+    id: '02',
+    nome: 'Avatar',
+    nota:'3,5',
+    imagem:'dbz.jpg',
+},
+{
+    id: '03',
+    nome: 'Desaparecida',
+    nota:'8.5',
+    imagem:'OnePiece.jpg'
+},
+{
+    id: '04',
+    nome:'Gato de Botas II',
+    nota:'9.8',
+    imagem:'Cavaleiros.jpg'
+},
+{
+    id: '05',
+    nome:'Pokemon',
+    nota:'9.8',
+    imagem:'Pokemon.jpg'
+},
+{
+    id: '06',
+    nome:'Os Incríveis',
+    nota:'9.8',
+    imagem:'Bakugan.jpg'
+},
+{
+    id: '07',
+    nome:'Super Mario Bros',
+    nota:'9.8',
+    imagem:'SuperOnze.jpg'
+},
+{
+    id: '08',
+    nome:'M3GAN',
+    nota:'9.8',
+    imagem:'DemonSlayer.jpg'
+},
+{
+    id: '09',
+    nome:'Pânico VI',
+    nota:'9.8',
+    imagem:'Beyblade.jpg'
+},
+{
+    id: '10',
+    nome:'Wandinha',
+    nota:'9.8',
+    imagem:'Jujutsu.jpg'
+},
+]
+
+export default DATA;
